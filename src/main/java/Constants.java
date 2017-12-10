b7b4255ce142a93c56ecebc84f784dc68e6a1f39
Anton Simeonov <antonmitkovsimeonov@gmail.com>
2017-12-10 16:13:36 +0200

    Defining a method for setup log for every class.

---
+++ src/main/java/Constants.java
@@ -0,0 +1,4 @@
+public class Constants
+{
+    public final static String LOG_FILE_PATH = "/home/asimeonov/Software/log/test.log";
+}
--- src/main/java/LogTest.java
+++ src/main/java/LogTest.java
@@ -1,19 +1,39 @@
 import java.io.IOException;
 import java.util.logging.*;
 
-public class LogTest {
-
+public class LogTest
+{
 
         private final static Logger logger = Logger.getLogger(LogTest.class.getName());
 
         public void log()
         {
 
-            Handler fh = null;  // append is true
+
+
+            logger.info("This is a info-level message");
+            logger.config("This is a config-level message");
+            logger.fine("This is a fine-level message");
+            logger.finer("This is a finer-level messag");
+            logger.finest("This is a finest-level message");  // below the logger's level
+
+
+        }
+
+        public void log2()
+        {
+            logger.info("Info log message");
+        }
+
+
+        public void setLogger()
+        {
+
+            Handler fh = null;
 
             try
             {
-                fh = new FileHandler("/home/asimeonov/Software/log/test.log", true);
+                fh = new FileHandler(Constants.LOG_FILE_PATH, false);
             }
             catch (IOException e)
             {
@@ -23,26 +43,7 @@ public class LogTest {
             fh.setFormatter(new SimpleFormatter());
 
             logger.addHandler(fh);
-
-            logger.setLevel(Level.FINE);
-
-            try
-            {
-                throw new Exception("Simulating an exception");
-            }
-            catch (Exception ex)
-            {
-                logger.log(Level.SEVERE, ex.getMessage(), ex);
-            }
-
-            logger.info("This is a info-level message");
-            logger.config("This is a config-level message");
-            logger.fine("This is a fine-level message");
-            logger.finer("This is a finer-level messag");
-            logger.finest("This is a finest-level message");  // below the logger's level
-
-            fh.flush();
-            fh.close();
+            logger.setLevel(Level.FINEST);
 
         }
 
